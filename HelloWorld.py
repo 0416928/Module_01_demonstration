bbba86c00e78f517e6f4e2a47f0da86c688cdcaf
Gagan <gkaur21.rrc.ca>
2024-08-31 02:03:47 -0500

    Added initial function to display Hello World!

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,6 +2,8 @@
 Description: {This program displays the text "Hello World"}
 Author:{Gaganpreet kaur}
 Date: { August 30,2024}
+Usage: usage details
 """
 
 print("Hello world")
+print("My name is Gaganpreet Kaur.");
